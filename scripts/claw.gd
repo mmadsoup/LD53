8c86dd2cf51690337aa6d5998a11c2fe67ab8952
Kise <33649578+kise08@users.noreply.github.com>
2023-04-29 17:50:41 -0700

    adding pixel art

--- scripts/claw.gd
+++ scripts/claw.gd
@@ -2,29 +2,24 @@ extends Node2D
 
 @onready var line_2d = $Line2D
 @onready var pivot = $Pivot
-@export var move_speed = 50
+@export var move_speed = 30
 @onready var claw = $Claw
 @onready var damped_spring_joint_2d = $DampedSpringJoint2D
-
-
 
 func _ready():
 	line_2d.add_point(pivot.position, 0)
 	line_2d.add_point(claw.position, 1)
+	line_2d.width = 1.0
 
 func _process(delta):
 
 	line_2d.set_point_position(0, pivot.position)
-	line_2d.set_point_position(1, claw.position)
+	line_2d.set_point_position(1, Vector2(claw.position.x, claw.position.y - 4))
 	
 	if Input.is_action_pressed('ui_left'):
 		pivot.position.x -= move_speed * delta
-#		tween_claw_angle(-30, 0.5)
 	elif Input.is_action_pressed('ui_right'):
 		pivot.position.x += move_speed * delta
-#		tween_claw_angle(30, 0.5)
-#	else:
-#		tween_claw_angle(0, 0.5)
 		
 	if Input.is_action_just_pressed('ui_select'):
 		if Globals.grabbed_item == null:
@@ -35,14 +30,9 @@ func _process(delta):
 			Globals.grab = false
 		
 		if Globals.grab:
-			damped_spring_joint_2d.rest_length = 500
-		else:
 			damped_spring_joint_2d.rest_length = 100
+		else:
+			damped_spring_joint_2d.rest_length = 5
 		
 	if Globals.grab and Globals.grabbed_item != null:
-		damped_spring_joint_2d.rest_length = 100
-
-
-func tween_claw_angle(angle, time):
-	var _t = create_tween()
-	_t.tween_property(claw, "rotation", deg_to_rad(angle), time)
+		damped_spring_joint_2d.rest_length = 5
--- scripts/mail.gd
+++ scripts/mail.gd
@@ -1,9 +1,6 @@
 extends RigidBody2D
 @onready var area_2d = $Area2D
 
-func _ready():
-	pass # Replace with function body.
-
 func _on_area_2d_body_entered(body):
 	if str(body).split(":")[0] == "Claw":
 		Globals.grabbed_item = self
