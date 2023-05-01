8d20bec7c93a39d7472385439b298dd367878f97
Kise <33649578+kise08@users.noreply.github.com>
2023-05-01 16:02:32 -0700

    designed game over screen

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -7,4 +7,5 @@ func _process(_delta):
 	label.text = str(ceil(timer.time_left))
 	
 	if timer.time_left <= 0:
+		#Globals.emit_signal('play_scene_transition', )
 		get_tree().change_scene_to_file('res://scenes/end.tscn')
